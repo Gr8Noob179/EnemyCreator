b600b80cef2b9ab743d2e9c0b0b5bacbef317752
Gr8Noob179 <erdakov18@gmail.com>
2024-05-21 06:05:21 -0400

    Crosshair and shooting system added

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private Transform gun;
+    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] private float maxSpeed = 17.5f;
+    [SerializeField] private float acceleration = 10f;
+    [SerializeField] private float bulletDmg = 5.0f;
+
+    private Vector2 lookingPosition;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lookingPosition = (mousePosition - (Vector2)gun.position).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity += bulletSpeed * lookingPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField, Range(0f, 100f)] private float acceleration = 5f;
-    [SerializeField, Range(0f, 100f)] private float deceleration = 5f;
-    [SerializeField, Range(0f, 100f)] private float maxSpeed = 10f;
-    [SerializeField, Range(0f, 100f)] private float stopThreshold = 0.1f;
+    [SerializeField] private GameObject bulletPref;
+    [SerializeField] private Transform crosshair;
+
+    [SerializeField] private float rotationOffset = 5.0f;
+
+    [SerializeField, Range(0f, 15f)] private float acceleration = 5f;
+    [SerializeField, Range(0f, 15f)] private float deceleration = 5f;
+    [SerializeField, Range(0f, 25f)] private float maxSpeed = 10f;
+    [SerializeField, Range(0f, 0.5f)] private float stopThreshold = 0.1f;
     [SerializeField] private Vector2 velocity;
     [SerializeField] private Vector2 desiredVelocity;
 
     private Rigidbody2D rb;
+    private Vector3 lookingDirection;
+    private float radius = 0.7f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +30,24 @@ public class PlayerController : MonoBehaviour
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        lookingDirection = (mousePosition - transform.position).normalized;
 
         desiredVelocity = new Vector2(inputX, inputY).normalized * maxSpeed;
+        if (Input.GetMouseButtonDown(0)) Shoot(lookingDirection);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Mouse position
+        #region
+
+        #endregion
+
+        //Player movement
+        #region
         Vector2 differenceVelocity = desiredVelocity - velocity;
 
         if (desiredVelocity.magnitude > 0.1f)
@@ -50,10 +69,30 @@ public class PlayerController : MonoBehaviour
         }
 
         rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        #endregion
+
+        //Crosshair
+        #region
+        float desiredAngle = Mathf.Atan2(lookingDirection.y, lookingDirection.x);
+
+        Vector3 dessiredPosition = new Vector3(
+                transform.position.x + radius * Mathf.Cos(desiredAngle),
+                transform.position.y + radius * Mathf.Sin(desiredAngle), 
+                0
+            );
+        crosshair.position = Vector3.Lerp(crosshair.position, dessiredPosition, 25f * Time.fixedDeltaTime);
+
+        crosshair.rotation = Quaternion.Euler(0, 0, (desiredAngle * Mathf.Rad2Deg) + rotationOffset);
+        #endregion
+
     }
 
-    private void PlayerRotation()
+    private void Shoot(Vector3 direction)
     {
+        float rotationAngle = Mathf.Atan2(lookingDirection.y, lookingDirection.x) * Mathf.Rad2Deg;
 
+        GameObject go = Instantiate(bulletPref, transform.position, Quaternion.Euler(0f, 0f, rotationAngle + 90f));
+
+        Destroy(go, 2.5f);
     }
 }
